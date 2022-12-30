caf2c9ea73b9341d14a27dc3bdc93e6e11559329
fa19bse082 <114099233+fa19bse082@users.noreply.github.com>
2022-12-30 22:08:23 +0500

    changes

--- lib/configMaps.dart
+++ lib/configMaps.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-String mapKey = "AIzaSyBZrop69vHrEqZkAJOQI8tmnmGVyXTl8p4";
+String mapKey = "";
 
 late User firebaseUser;
 late User userCurrentInfo;
